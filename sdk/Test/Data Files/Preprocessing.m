e9baad440eecd00717dcf8df740c6be1d22dbb56
Krittisak Chaiyakul <krittisak2006@gmail.com>
2017-08-15 19:48:16 -0500

    Add TraningNetwork.m

--- sdk/Test/Data Files/Preprocessing.m
+++ sdk/Test/Data Files/Preprocessing.m
@@ -1,38 +1,51 @@
 %% Preprocessing.m 
- 
+
+options = 1; % 1 for 6 outputs; 2 for 4 outputs; 3 for 4 outputs with merging
 enable_plot = false;
 subinterval_time = 0.2;
  
-%% offset estimate between video and Myo 
-vid_mark = [148/20, 202/20, 264/20]; % software estimates at 30 FPS.
-guess_mark = [2.461, 5.523, 8.579];
+%% offset estimate between video and Myo
+vid_mark = [294/20, 331/20, 368/20];
+guess_mark = [2.156, 3.937, 5.883];
 offset = mean(vid_mark - guess_mark); % Myo is ~ 4.7s delayed.
  
+%% Set file's names
+
+folder_name = '07313';
+str_file = '0731EricHTherblig';
+str_acc_w = 'worldAccelH';
+str_emg = 'emgH';
+
 %% Close figures
  
 fh=findall(0,'type','figure');
 for i=1:length(fh)
      clo(fh(i));
 end
- 
+
 %% Label files
- 
-fileID = fopen('07312/0731KrittisakHTherblig.txt','r');
+
+fileID = fopen(strcat(folder_name, '/',str_file,'.txt'),'r');
  
 %% File for world oriented Myo Accel Reading - 50 Hz 
  
-str_acc_w = 'worldAccelH';
-base_acc_w = csvread(strcat('07312/',str_acc_w,'.csv'));
-base_acc_w(:,3) = base_acc_w(:,3) - 9.8;
- 
+base_acc_w = csvread(strcat(folder_name, '/',str_acc_w,'.csv'));
+
 Fsa = 50;
 length_acc_w = size(base_acc_w,1);
 num_acc_signals_w = size(base_acc_w,2);
+
+% base_acc_w(:,3) = base_acc_w(:,3) - 9.8;
+for i=1:num_acc_signals_w
+    mu = mean (base_acc_w (:,i));
+    for j=1:length_acc_w
+        base_acc_w (j,i) = base_acc_w (j,i) - mu;
+    end
+end
  
 %% File for Myo EMG Reading - 200 Hz 
 % 8 emg sensors 
-str_emg = 'emgH';
-base_emg = csvread(strcat('07312/',str_emg,'.csv'));
+base_emg = csvread(strcat(folder_name, '/',str_emg,'.csv'));
  
 length_emg = size(base_emg,1);
 num_emg_signals = size(base_emg,2);
@@ -130,7 +143,7 @@ end
 mag_acc = temp;
  
 %% Plot Filtered EMG Data
- 
+
 if enable_plot
     f = figure(1);
     p = uipanel('Parent',f,'BorderType','none'); 
@@ -228,9 +241,11 @@ while ischar(temp)
         name = 6;
     end
     
-    therbligs{end + 1, 1} = start_time;
-    therbligs{end, 2} = end_time;
-    therbligs{end, 3} = name;
+    if (name <= 4) | ((options == 1) & (name <= 6))
+        therbligs{end + 1, 1} = start_time;
+        therbligs{end, 2} = end_time;
+        therbligs{end, 3} = name;
+    end
     
     temp = fgetl(fileID);
 end
@@ -239,62 +254,64 @@ end
  
 length_therbligs = size (therbligs, 1);
 
-for num=2:9%length_therbligs
-    name = therbligs{num,3};
-    if name == 1
-        name = 'Rest';
-    elseif name == 2
-        name = 'Transport_Empty';
-    elseif name == 3
-        name = 'Transport_Loaded';
-    elseif name == 4
-        name = 'Hold';
-    elseif name == 5
-        name = 'Grasp';
-    elseif name == 6
-        name = 'Release_Load';
-    end
-    
-    f = figure(1+num);
-    p = uipanel('Parent',f,'BorderType','none'); 
-    p.Title = [name]; 
-    p.TitlePosition = 'centertop'; 
-    p.FontSize = 12;
-    p.FontWeight = 'bold';
-    
-    time_start = therbligs{num,1} - offset;
-    time_end = therbligs{num,2} - offset;
-    frame_start = round (time_start * 50);
-    frame_end = round (time_end * 50);
-    frame_length = frame_end - frame_start + 1;
-    
-    %% Plot Filtered EMG Data
-    for i=1:num_emg_signals
-        ax = subplot(12,1,i,'Parent',p);
-        y_max=y_max_e(i);
-        x=linspace(time_start,time_end,frame_length);
-        plot(ax,x,filt_emg_signal_avg(frame_start:frame_end,i));
-        axis([time_start,time_end,0,y_max]);
-        ylabel(strcat ('emg ', int2str (i)));
-    end
-    
-    %% Plot Raw Acceleration Data
-    for i=1:num_acc_signals_w
-        ax = subplot(12,1,8+i,'Parent',p);
-        y_max=5;
-        x_a=linspace(time_start,time_end,frame_length);
-        plot(ax,x_a,base_acc_w(frame_start:frame_end,i));
-        axis([time_start,time_end,-y_max,y_max]);
-        ylabel(strcat ('acc ', int2str (i)));
-    end
+if enable_plot
+    for num=2:2%length_therbligs
+        name = therbligs{num,3};
+        if name == 1
+            name = 'Rest';
+        elseif name == 2
+            name = 'Transport_Empty';
+        elseif name == 3
+            name = 'Transport_Loaded';
+        elseif name == 4
+            name = 'Hold';
+        elseif name == 5
+            name = 'Grasp';
+        elseif name == 6
+            name = 'Release_Load';
+        end
 
-    %% Plot Acceleration Magnitude
-    ax = subplot (12,1,12,'Parent',p);
-    y_max=5;%max(mag_acc);
-    x_a=linspace(time_start,time_end,frame_length);
-    plot(ax,x_a,mag_acc(frame_start:frame_end));
-    axis([time_start,time_end,0,y_max]);
-    ylabel('mag acc');
+        f = figure(1+num);
+        p = uipanel('Parent',f,'BorderType','none'); 
+        p.Title = [name]; 
+        p.TitlePosition = 'centertop'; 
+        p.FontSize = 12;
+        p.FontWeight = 'bold';
+
+        time_start = therbligs{num,1} - offset;
+        time_end = therbligs{num,2} - offset;
+        frame_start = round (time_start * 50);
+        frame_end = round (time_end * 50);
+        frame_length = frame_end - frame_start + 1;
+
+        %% Plot Filtered EMG Data
+        for i=1:num_emg_signals
+            ax = subplot(12,1,i,'Parent',p);
+            y_max=y_max_e(i);
+            x=linspace(time_start,time_end,frame_length);
+            plot(ax,x,filt_emg_signal_avg(frame_start:frame_end,i));
+            axis([time_start,time_end,0,y_max]);
+            ylabel(strcat ('emg ', int2str (i)));
+        end
+
+        %% Plot Raw Acceleration Data
+        for i=1:num_acc_signals_w
+            ax = subplot(12,1,8+i,'Parent',p);
+            y_max=5;
+            x_a=linspace(time_start,time_end,frame_length);
+            plot(ax,x_a,base_acc_w(frame_start:frame_end,i));
+            axis([time_start,time_end,-y_max,y_max]);
+            ylabel(strcat ('acc ', int2str (i)));
+        end
+
+        %% Plot Acceleration Magnitude
+        ax = subplot (12,1,12,'Parent',p);
+        y_max=5;%max(mag_acc);
+        x_a=linspace(time_start,time_end,frame_length);
+        plot(ax,x_a,mag_acc(frame_start:frame_end));
+        axis([time_start,time_end,0,y_max]);
+        ylabel('mag acc');
+    end
 end
  
 if enable_plot
@@ -700,6 +717,23 @@ y_data = [];
 
 id = 1;
 
+for i=1:num_emg_signals
+    mx = max (filt_emg_signal_avg(:,i));
+    for j=1:length_emg_avg
+        filt_emg_signal_avg(j,i) = filt_emg_signal_avg(j,i) / mx;
+    end
+end
+for i=1:num_acc_signals_w
+    mx = max (base_acc_w(:,i));
+    for j=1:length_acc_w
+        base_acc_w(j,i) = base_acc_w(j,i) / mx;
+    end
+end
+mx = max (mag_acc(:));
+for j=1:length_acc_w
+    mag_acc(j) = mag_acc(j) / mx;
+end
+
 for num=1:length_therbligs
     label = therbligs{num,3};
     
@@ -714,14 +748,23 @@ for num=1:length_therbligs
 
         X = [];
         for i=1:num_emg_signals
-            X = [X; filt_emg_signal_avg(st:ed,i)];
+            mu = mean (filt_emg_signal_avg(st:ed,i));
+            X = [X; mu];
+%             X = [X; filt_emg_signal_avg(st:ed,i)];
         end
         for i=1:num_acc_signals_w
-            X = [X; base_acc_w(st:ed,i)];
+            mu = mean (base_acc_w(st:ed,i));
+            X = [X; mu];
+%             X = [X; base_acc_w(st:ed,i)];
         end
-        X = [X; mag_acc(st:ed)];
+        mu = mean (mag_acc(st:ed));
+        X = [X; mu];
+%         X = [X; mag_acc(st:ed)];
         
         y = zeros (6, 1);
+        if options ~= 1
+            y = zeros (4, 1);
+        end
         y (label) = 1;
         
         X_data = [X_data, X];
@@ -730,28 +773,7 @@ for num=1:length_therbligs
     end
 end
 
-numInputs = 1;
-numLayers = 2;
-biasConnect = [1; 1];
-inputConnect = [1; 0];
-layerConnect = [0 0; 1 0];
-outputConnect = [0 1];
+%% Save data as csv file
 
-net = network(numInputs,numLayers,biasConnect,inputConnect,layerConnect,outputConnect);
-
-net.inputs{1}.size = size (X_data, 1);
-
-net.layers{1}.size = 100;
-net.layers{1}.transferFcn = 'tansig';
-net.layers{1}.initFcn = 'initnw';
-
-net.layers{2}.size = 6;
-net.layers{2}.transferFcn = 'logsig';
-net.layers{2}.initFcn = 'initnw';
-
-net.initFcn = 'initlay';
-net.performFcn = 'crossentropy';
-net.trainFcn = 'trainscg';
-
-net = init (net);
-net = train (net, X_data, y_data);
+csvwrite(strcat(str_file,'_X.csv'), X_data);
+csvwrite(strcat(str_file,'_y.csv'), y_data);
